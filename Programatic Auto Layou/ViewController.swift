432d8537ff17b169f234f5170ee61838f208cab9
Robert Graman <rjgxfv@mail.missouri.edu>
2019-02-15 14:39:16 -0600

    Initial Commit

---
+++ Programatic Auto Layou/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Programatic Auto Layou
+//
+//  Created by Robert Graman on 2/15/19.
+//  Copyright © 2019 Robert Graman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
